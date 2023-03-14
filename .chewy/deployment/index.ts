4b4c8be335d132dcc96c7a5f2a79a920f10ebb12
Raphaël Titsworth-Morin <raphael@raphaeltm.com>
2023-03-14 18:23:04 +0000

    global network for dev

--- .chewy/deployment/index.ts
+++ .chewy/deployment/index.ts
@@ -1,1 +1,11 @@
 import * as pulumi from "@pulumi/pulumi";
+import * as docker from "@pulumi/docker";
+import * as chewy from "@gochewy/lib";
+
+const project = chewy.project.getProjectConfig();
+const id = `${project.name}-${project.id}`;
+
+export const networkName = `${id}-network`
+const network = new docker.Network(networkName, {
+    name: networkName,
+});
